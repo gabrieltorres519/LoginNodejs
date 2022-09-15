b4e1348c27df23411348c827a9e8db23857826b8
unknown <gabrieltorres9909@gmail.com>
2022-09-15 18:25:03 -0500

    Encriptado de contraseña de forma gratuita

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
     email: String,
     password: String
 });
+
+userSchema.method.encryptPassword = (passport)=>{ // Se encripta el password que se reciba del cliente 
+    return bcrypt.hashSync(password,bcrypt.genSaltSync(10)); // Se encripta la contraseña y la cantidad de milisegundos entre semilla y semilla 
+// Comisión Bancaria y de Valores la toma como un método de encriptación válido 
+};
 
 
 module.exports = mongoose.model('users',userSchema);
