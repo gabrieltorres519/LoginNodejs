6d06700443c8b4bc528994bd33f56395842dca43
unknown <gabrieltorres9909@gmail.com>
2022-09-15 18:37:02 -0500

    Agregando segunda opción de encriptado

--- src/models/user.js
+++ src/models/user.js
@@ -7,9 +7,13 @@ const userSchema = new Schema({
     password: String
 });
 
-userSchema.method.encryptPassword = (passport)=>{ // Se encripta el password que se reciba del cliente 
+userSchema.methods.encryptPassword = (passport)=>{ // Se encripta el password que se reciba del cliente 
     return bcrypt.hashSync(password,bcrypt.genSaltSync(10)); // Se encripta la contraseña y la cantidad de milisegundos entre semilla y semilla 
 // Comisión Bancaria y de Valores la toma como un método de encriptación válido 
+};
+
+userSchema.methods.comparePassword = (passport)=>{ 
+    return bcrypt.compareSync(password,this.passport); // Se compara la contraseña enviada por sformulario con la que se encuentra en la base de datos 
 };
 
 
--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -2,9 +2,35 @@ const passport = require('passport');
 const localStrategy = require('passport-local').Strategy; 
 //Strategy simple para la autenticación local (encriptado)
 
+const User = require('../models/user');
+
+// Opción 1 con try catch 
+
+// passport.use('local-signup', new localStrategy({
+//     usernameField: 'email', //Nombre del campo en el formulario que se usará para la autenticación ('name' en el formulairo)
+//     passwordField: 'password',
+//     passReqToCallback:true // Una ves verificados los campos en el formulario se realiza una autenticación por método y no por token (file)
+// },(req, email, password, done)=>{
+//     const user = new User(); // Creando objeto User usando el modelo recién creado
+//     user.email = email;
+//     user.password = password; // Pasamos al modelo los datos recibidos en el formulario
+//     user.save()
+//     .then(db => console.log('Usuario registrado'))
+//     .catch(err => console.error(err)); // Verificamos si se logró la conexión 
+// } // Si son correctos 'done' y se ejecuta lo de la función flecha 
+// )); //Asegurar usuario y contraseña (el nombre local-signup lo ponemos nosotros)
+
+
+//Opción asíncrona moderna de javascript
 passport.use('local-signup', new localStrategy({
     usernameField: 'email', //Nombre del campo en el formulario que se usará para la autenticación ('name' en el formulairo)
     passwordField: 'password',
     passReqToCallback:true // Una ves verificados los campos en el formulario se realiza una autenticación por método y no por token (file)
-},(req, email, password, done)=>{} // Si son correctos 'done' y se ejecuta lo de la función flecha 
+},async (req, email, password, done)=>{
+    const user = new User(); // Creando objeto User usando el modelo recién creado
+    user.email = email;
+    user.password = password; // Pasamos al modelo los datos recibidos en el formulario
+    await user.save()
+    done(null,use)
+} // Si son correctos 'done' y se ejecuta lo de la función flecha 
 )); //Asegurar usuario y contraseña (el nombre local-signup lo ponemos nosotros)
