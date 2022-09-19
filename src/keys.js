6ff3b0462c98d7efdbecebd3b67f5843beb5a9b8
unknown <gabrieltorres9909@gmail.com>
2022-09-18 21:26:07 -0500

    Agregada cadena de conexión a contenedor de mongo en local temporelmente

--- src/keys.js
+++ src/keys.js
@@ -2,6 +2,6 @@ module.exports ={
     mongodb:{
         // URL: 'mongodb+srv://admin:<password>@cluster0.nhslgpi.mongodb.net/?retryWrites=true&w=majority'
         // URI: 'mongodb+srv://admin:root@cluster0.nhslgpi.mongodb.net/?retryWrites=true&w=majority'
-        URI: 'mongodb://mongo/mibase'
+        URI: 'mongodb://localhost/mibase'
     } //Cadena de conexión de la base de datos 
 }
